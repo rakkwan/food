5589d95763e33bf78d71354b6f263c4b5ae3891a
Jittima Goodrich <jgoodrich4@mail.greenriver.edu>
2019-04-15 17:45:44 -0400

    Add form1

--- index.php
+++ index.php
@@ -22,9 +22,86 @@
     // define a default route
     $f3->route('GET /', function()
     {
-        //echo '<h1>Hola!</h1>';
+        //echo '<h1>Food!</h1>';
         $view = new Template();
         echo $view->render('views/home.html');
+    });
+
+    // Define a breakfast route
+
+    $f3->route('GET /breakfast', function()
+    {
+        //echo "<h1>Breakfast Page</h1>";
+        // display breakfast view
+        $view = new Template();
+        echo $view->render('views/breakfast.html');
+    });
+
+    $f3->route('GET /lunch', function()
+    {
+        // display a lunch views
+        $view = new Template();
+        echo $view->render('views/lunch.html');
+    });
+
+    $f3->route('GET /breakfast/continental', function()
+    {
+        // display a bfast views
+        $view = new Template();
+        echo $view->render('views/bfast-cont.html');
+    });
+
+    $f3->route('GET /lunch/brunch/buffet', function()
+    {
+        // display a brunch views
+        $view = new Template();
+        echo $view->render('views/buffet.html');
+    });
+
+    // Define a route with a parameter
+    $f3->route('GET /@item', function($f3, $params)
+    {
+        $item = $params['item'];
+        $foodsWeServe = array('spaghetti', 'enchiladas',
+            'pad thai', 'lumpia');
+
+        if (!in_array($item, $foodsWeServe))
+        {
+            echo "We don't serve $item";
+        }
+
+        switch ($item)
+        {
+            case 'spaghetti':
+                echo "<h3>I like $item with meatballs.</h3>";
+                break;
+            case 'pizza':
+                echo "<h3>Pepperoni or veggie?</h3>";
+                break;
+            case 'tacos':
+                echo "<h3>We don't have $item.</h3>";
+                break;
+            case 'bagel':
+                $f3->reroute("/breakfast/continental");
+            default:
+                $f3->error(404);
+        }
+
+    });
+
+    $f3->route('GET /@first/@last', function($f3, $params)
+    {
+        $first = ucfirst($params['first']);
+        $last = ucfirst($params['last']);
+
+        echo "<h1>Hello $first $last</h1>";
+    });
+
+    $f3->route('GET /order', function()
+    {
+        // display a brunch views
+        $view = new Template();
+        echo $view->render('views/form1.html');
     });
 
     // Run Fat-Free
